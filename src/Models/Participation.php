8876a315bca679c4a45cc409c95c69b2d08e30d3
Azazide <support@pixstation.cc>
2024-07-23 17:03:31 +0300

    chat
    Using to chat project

---
+++ src/Models/Participation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Musonza\Chat\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+//use Illuminate\Database\Eloquent\SoftDeletes;
+use Musonza\Chat\BaseModel;
+use Musonza\Chat\ConfigurationManager;
+
+class Participation extends BaseModel
+{
+//    use SoftDeletes;
+
+    protected $table = ConfigurationManager::PARTICIPATION_TABLE;
+
+    protected $fillable = [
+        'messageable_id',
+        'messageable_type',
+        'conversation_id',
+        'settings',
+    ];
+
+    protected $casts = [
+        'settings' => 'array',
+    ];
+
+    /**
+     * Conversation.
+     *
+     * @return BelongsTo
+     */
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class, 'conversation_id');
+    }
+
+    public function messageable()
+    {
+        return $this->morphTo()->with('participation');
+    }
+}
